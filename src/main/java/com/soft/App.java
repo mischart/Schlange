4842e859b41c746be6d4f6c3e857977a76a2c8fc
Artur Nowodworski <a.nowo@t-online.de>
2020-11-21 14:20:37 +0100

    First commit

---
+++ src/main/java/com/soft/App.java
@@ -0,0 +1,18 @@
+package com.soft;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+}
