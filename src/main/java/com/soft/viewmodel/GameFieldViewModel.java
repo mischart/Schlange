43612171ffca6cc0a6c2253d780cf2c574775653
Artur Nowodworski <a.nowo@t-online.de>
2020-11-23 21:16:41 +0100

    Create a class GameFieldViewModel

---
+++ src/main/java/com/soft/viewmodel/GameFieldViewModel.java
@@ -0,0 +1,11 @@
+package com.soft.viewmodel;
+
+import com.soft.model.GameField;
+
+public class GameFieldViewModel {
+    private final GameField gameField;
+
+    public GameFieldViewModel(GameField gameField) {
+        this.gameField = gameField;
+    }
+}
