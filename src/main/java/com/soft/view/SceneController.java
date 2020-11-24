62860c9a0a09499f06f2360bbc37428949814d8e
Artur Nowodworski <a.nowo@t-online.de>
2020-11-24 21:50:26 +0100

    Implement the ViewSwitch in the SceneController

--- src/main/java/com/soft/view/SceneController.java
+++ src/main/java/com/soft/view/SceneController.java
@@ -4,12 +4,17 @@ import javafx.scene.Scene;
 
 import java.util.EnumMap;
 
-public class SceneController {
+public class SceneController implements ViewSwitch {
     private final EnumMap<ViewType, View> viewEnumMap;
     private final Scene scene;
 
     public SceneController(EnumMap<ViewType, View> viewEnumMap, Scene scene) {
         this.viewEnumMap = viewEnumMap;
         this.scene = scene;
+    }
+
+    @Override
+    public void activateView(ViewType viewType) {
+        scene.setRoot(viewEnumMap.get(viewType).getRoot());
     }
 }
