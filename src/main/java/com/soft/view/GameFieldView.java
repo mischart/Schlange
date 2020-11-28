8da58d0bd9597416f1125be469ef658ad5e61367
Artur Nowodworski <a.nowo@t-online.de>
2020-11-28 21:29:12 +0100

    Show the snake in the GameFieldView

--- src/main/java/com/soft/view/GameFieldView.java
+++ src/main/java/com/soft/view/GameFieldView.java
@@ -1,17 +1,38 @@
 package com.soft.view;
 
 import com.soft.viewmodel.GameFieldViewModel;
+import com.soft.viewmodel.ViewModelPoint;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class GameFieldView extends View {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameFieldView extends View implements Initializable {
 
     private final GameFieldViewModel viewModel;
     private int gameObjectWidth;
     private int gameObjectHeight;
+    private ObservableList<Rectangle> snakeBody;
 
     @FXML
     private Pane gameFieldRoot;
+    private final ListChangeListener<Rectangle> gameObjectListListener = change -> {
+        while (change.next()) {
+            if (change.wasAdded()) {
+                gameFieldRoot.getChildren().addAll(change.getAddedSubList());
+            }
+            if (change.wasRemoved()) {
+                gameFieldRoot.getChildren().removeAll(change.getRemoved());
+            }
+        }
+    };
 
     public GameFieldView(GameFieldViewModel viewModel) {
         this.viewModel = viewModel;
@@ -34,6 +55,20 @@ public class GameFieldView extends View {
     @Override
     public ViewType getViewType() {
         return ViewType.GAME_FIELD;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        snakeBody = FXCollections.observableArrayList();
+        snakeBody.addListener(gameObjectListListener);
+        viewModel.getSnakeBody().forEach(viewModelPoint -> snakeBody.add(createGameObject(viewModelPoint, Color.BLACK)));
+    }
+
+    private Rectangle createGameObject(ViewModelPoint viewModelPoint, Color color) {
+        Rectangle rectangle = new Rectangle(gameObjectWidth, gameObjectHeight, color);
+        rectangle.xProperty().bind(viewModelPoint.xProperty().multiply(gameObjectWidth));
+        rectangle.yProperty().bind(viewModelPoint.yProperty().multiply(gameObjectHeight));
+        return rectangle;
     }
 
 }
