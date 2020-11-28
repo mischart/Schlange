183cd26e5b696fc9c620774f8df45bbf8b104116
Artur Nowodworski <a.nowo@t-online.de>
2020-11-28 19:14:29 +0100

    Add a field snakeBody to the GameFieldViewModel

--- src/main/java/com/soft/viewmodel/GameFieldViewModel.java
+++ src/main/java/com/soft/viewmodel/GameFieldViewModel.java
@@ -1,11 +1,20 @@
 package com.soft.viewmodel;
 
 import com.soft.model.GameField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class GameFieldViewModel {
     private final GameField gameField;
+    private final ObservableList<ViewModelPoint> snakeBody;
 
     public GameFieldViewModel(GameField gameField) {
         this.gameField = gameField;
+        snakeBody = FXCollections.observableArrayList();
+        gameField.getSnakeBody().forEach(fieldPoint -> snakeBody.add(new ViewModelPoint(fieldPoint.getX(), fieldPoint.getY())));
+    }
+
+    public ObservableList<ViewModelPoint> getSnakeBody() {
+        return FXCollections.unmodifiableObservableList(snakeBody);
     }
 }
