e61163b6b8d5b85156abee2960478c0dc637176d
Artur Nowodworski <a.nowo@t-online.de>
2020-12-10 20:16:33 +0100

    Add a field to the View

--- src/main/java/com/soft/view/View.java
+++ src/main/java/com/soft/view/View.java
@@ -5,10 +5,15 @@ import javafx.scene.layout.Pane;
 
 public abstract class View {
 
+    ViewSwitch viewSwitch;
+
     abstract Pane getRoot();
 
     abstract ViewType getViewType();
 
     abstract void keyPressed(KeyCode keyCode);
 
+    public void setViewSwitch(ViewSwitch viewSwitch) {
+        this.viewSwitch = viewSwitch;
+    }
 }
